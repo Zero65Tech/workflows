fa4ba9744e6dd5b6d74c2515820c5796082a2e78
Prashant Gupta <antshpra@gmail.com>
2024-12-09 06:12:25 +0000

    services

--- src/models/task.js
+++ src/models/task.js
@@ -4,12 +4,12 @@ exports.add = Joi.object({
   executionId: Joi.string().required(),
   step: Joi.string().required(),
   task: Joi.string().required(),
-  state: Joi.string().valid('queued', 'in_progress', 'error').required(),
+  state: Joi.string().valid('pending', 'queued', 'in_progress', 'error').required(),
   created: Joi.date().iso().required(),
   updated: Joi.date().iso().required()
 }).required();
 
 exports.update = Joi.object({
-  state: Joi.string().valid('queued', 'in_progress', 'error').required(),
+  state: Joi.string().valid('pending', 'queued', 'in_progress', 'error').required(),
   updated: Joi.date().iso().required()
 }).required();
---
+++ src/services/index.js
@@ -0,0 +1,48 @@
+
+exports.createNewWorkflow = async (name, owner) => {
+
+  const data = { name, owner, created: new Date(), updated: new Date };
+  const workflowId = await Workflow.create(data);
+
+  const versionId = await exports.updateWorkflow(workflowId, 'sample workflow', '[]', '[]');
+
+  return { workflowId, versionId };
+
+}
+  
+exports.updateWorkflow = async (workflowId, name, params, steps) => {
+
+  const checksum = Utils.generateChecksum({ params: JSON.parse(params), steps: JSON.parse(steps) });
+
+  const version = Version.getLatestByChecksum(workflowId, checksum);
+  
+  if (version) {
+    const data = { name, params, steps, updated: new Date() };
+    await Version.update(workflowId, version.id, data);
+    return version.id;
+  } else {
+    const data = { name, params, steps, checksum, created: new Date(), updated: new Date() }
+    return await Version.create(workflowId, data);
+  }
+
+}
+
+exports.executeWorkflow = async (workflowId, params) => {
+
+  const version = Version.getLatest(workflowId);
+
+  return await Execution.create(workflowId, version.id, params);
+
+}
+
+exports.processTasks = async () => {
+  const executions = Execution.getAllAtive();
+  for(const execution of executions) {
+    const version = Version.getLatest(workflowId);
+    // iterate over steps
+    // if task(s) is already created, update task status
+    // create task for next step
+    await Task.create(execution.workflowId, { executionId: execution.id, status: 'pending', host: 'localhost', params: execution.params });
+    // if all steps are completed, update execution status
+  }
+}
