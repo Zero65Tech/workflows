59dd08359ecc8dcb5aaefaa075968be5d5b6aa31
Prashant Gupta <antshpra@gmail.com>
2024-12-22 11:50:03 +0530

    devcontainer script

--- .devcontainer/script.sh
+++ .devcontainer/script.sh
@@ -2,11 +2,9 @@
 
 echo "@zero65:registry=https://asia-south1-npm.pkg.dev/zero65/npm/" > ~/.npmrc
 
-mkdir -p ~/.config/gcloud
 if [ -n "$GOOGLE_APPLICATION_CREDENTIALS_BASE64" ]; then
+    mkdir -p ~/.config/gcloud
     echo "$GOOGLE_APPLICATION_CREDENTIALS_BASE64" | base64 --decode > ~/.config/gcloud/application_default_credentials.json
-elif [ -n "$GOOGLE_APPLICATION_CREDENTIALS_JSON" ]; then
-    echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > ~/.config/gcloud/application_default_credentials.json
 fi
 
 npm update && npm run dev
